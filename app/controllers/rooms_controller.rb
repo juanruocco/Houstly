4fa1b7874f71e0d3a88390540cc5e71beaf9c004
Juan Montenegro <juanpabloruocco@gmail.com>
2017-10-19 11:30:33 -0500

    Implement edit and delete funcionalities for rooms and tours

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,32 +1,41 @@
 class RoomsController < ApplicationController
   def index
+    @room = Room.new
   end
 
   def new
+    @room = Room.new
   end
 
   def create
-    room = Room.new(room_params)
+    @room = Room.new(room_params)
     current_place.rooms << room
     redirect_to '/rooms'
-
   end
 
   def edit
-    @place = Place.find(params[:id])
+    @room = Room.find(params[:id])
   end
 
   def update
-    @place = Place.find(params[:id])
+    @room = Room.find(params[:id])
 
-    if @place.update_attributes(place_params)
-      flash[:success] = "Place added!"
+    if @room.update_attributes(room_params)
+      flash[:success] = "room updated!"
       redirect_to rooms_path and return
       #redirect_to people_path, notice: "#{first_name} #{last_name} has been updated!" and return
     end
 
     render 'edit'
   end
+
+  def destroy
+      @room = Room.find(params[:id])
+      @room.destroy
+      flash[:success] = "Room was deleted!"
+      #redirect_to people_path, notice: "#{first_name} #{last_name} has been deleted!" and return
+      redirect_to rooms_path
+    end
 
   private
   def room_params
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,16 +1,41 @@
 class ToursController < ApplicationController
   def index
+    @tour = Tour.new
   end
 
   def new
+    @tour = Tour.new
   end
 
   def create
-    tour = Tour.new(tour_params)
-    current_place.tours << tour
-    redirect_to '/tours'
-
+    @tour = Tour.new(tour_params)
+    current_place.tours << @tour
+    redirect_to tours_path
   end
+
+  def edit
+    @tour = Tour.find(params[:id])
+  end
+
+  def update
+    @tour = Tour.find(params[:id])
+
+    if @tour.update_attributes(tour_params)
+      flash[:success] = "tour updated!"
+      #redirect_to tours_path
+      #redirect_to people_path, notice: "#{first_name} #{last_name} has been updated!" and return
+    end
+
+    redirect_to tours_path
+  end
+
+  def destroy
+      @tour = Tour.find(params[:id])
+      @tour.destroy
+      flash[:success] = "tour was deleted!"
+      #redirect_to people_path, notice: "#{first_name} #{last_name} has been deleted!" and return
+      redirect_to tours_path
+    end
 
   private
   def tour_params
