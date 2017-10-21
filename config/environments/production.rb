3a14c1271f5d8f443b664ad82d7403502fa35d6d
Juan Montenegro <juanpabloruocco@gmail.com>
2017-10-20 20:15:23 -0500

    test to test heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,21 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-
-
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-config.serve_static_assets = false
-
-# Compress JavaScripts and CSS
-config.assets.compress = true
-
-# Don't fallback to assets pipeline if a precompiled asset is missed
-config.assets.compile = false
-
-# Generate digests for assets URLs
-config.assets.digest = true
-
-
+  config.serve_static_assets = true
+  config.assets.compile = true
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
