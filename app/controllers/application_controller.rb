d1bf48ff7194384fa699de5dca0c4c43503f35c0
tomasmontenegro14 <32658643+tomasmontenegro14@users.noreply.github.com>
2017-10-26 00:37:09 -0500

    Ajustes de diseño en los formularios
    Ajuste de diseño en los formularios y que estos queden enlazados por links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def current_place
-    @current_place = Place.first
+    @current_place = Place.find_by(:name_id => "finca")
   end
 
   helper_method :current_place
