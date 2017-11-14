a91c8f03cab3f075dfbaa6ba161e6f3b0f021175
Juan Montenegro <juanpabloruocco@gmail.com>
2017-11-14 18:52:36 -0500

    fix delete method of imagedescriptions

--- app/controllers/imagedescriptions_controller.rb
+++ app/controllers/imagedescriptions_controller.rb
@@ -1,4 +1,5 @@
 class ImagedescriptionsController < ApplicationController
+
   def index
     @place = Place.find(params[:place_id])
     @webcomponent = Webcomponent.find(params[:webcomponent_id])
@@ -8,8 +9,15 @@ class ImagedescriptionsController < ApplicationController
 
   def update
     @imagedescription = Imagedescription.find(params[:id])
-    if @imagedescription.update_attributes(imagedescription_params)
-      flash[:success] = "place updated!"
+    if params[:commit] == 'Update'
+      if @imagedescription.update_attributes(imagedescription_params)
+        flash[:success] = "place updated!"
+      end
+
+    elsif params[:commit] == 'Delete'
+      @imagedescription.destroy
+      flash[:success] = "Image Description was deleted!"
+
     end
     redirect_to place_webcomponent_imagedescriptions_path
   end
@@ -18,21 +26,10 @@ class ImagedescriptionsController < ApplicationController
     @place = Place.find(params[:place_id])
     @webcomponent = Webcomponent.find(params[:webcomponent_id])
     @imagedescription = Imagedescription.new(imagedescription_params)
-
-
     @webcomponent.imagedescriptions << @imagedescription
     redirect_to place_webcomponent_imagedescriptions_path
     return
   end
-
-  def destroy
-      @imagedescription = Imagedescription.find(params[:id])
-      @imagedescription.destroy
-      flash[:success] = "Image Description was deleted!"
-      #redirect_to people_path, notice: "#{first_name} #{last_name} has been deleted!" and return
-      redirect_to place_webcomponents_path
-  end
-
 
   private
 
